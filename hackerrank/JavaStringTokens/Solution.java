1cfd1954f4a172c8ad21501326878c56563ef637
Marek <marek@pw.edu.pl>
2020-05-12 23:48:22 +0200

    another solutions added

---
+++ hackerrank/JavaStringTokens/Solution.java
@@ -0,0 +1,22 @@
+package hackerrank.JavaStringTokens;
+
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        if (!scan.hasNext()) {
+            System.out.println(0);
+        } else {
+            String s = scan.nextLine();
+            String[] stringsS = s.trim().split("[ !,?._'@]+");
+            System.out.println(stringsS.length);
+            for (String item : stringsS) {
+                System.out.println(item);
+            }
+        }
+        scan.close();
+    }
+
+}
---
+++ hackerrank/PatternSyntaxChecker/Solution.java
@@ -0,0 +1,28 @@
+package hackerrank.PatternSyntaxChecker;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+import java.util.regex.*;
+
+public class Solution
+{
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        int testCases = Integer.parseInt(in.nextLine());
+        List<String> list = new LinkedList<>();
+        while (testCases > 0) {
+            String pattern = in.nextLine();
+            try {
+                Pattern.compile(pattern);
+                list.add("Valid");
+                testCases--;
+            } catch (PatternSyntaxException e) {
+                list.add("Invalid");
+                testCases--;
+            }
+        }
+        list.forEach(System.out::println);
+    }
+}
